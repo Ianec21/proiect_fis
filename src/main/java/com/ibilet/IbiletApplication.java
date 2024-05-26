b89ee089e91dd4bc23c79658d2f358d09d9c45db
Ianec21 <jizdan.ianec17@gmail.com>
2024-05-26 23:56:45 +0300

    Finished firebase integration

--- src/main/java/com/ibilet/IbiletApplication.java
+++ src/main/java/com/ibilet/IbiletApplication.java
@@ -1,13 +1,37 @@
 package com.ibilet;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
 
 @SpringBootApplication
 public class IbiletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IbiletApplication.class, args);
+		File file = new File(IbiletApplication.class.getClassLoader().getResource("firebase_credentials.json").getFile());
+
+		try {
+			FileInputStream firebaseCredentials = new FileInputStream(file.getAbsolutePath());
+
+			//check if there are no created apps already, otherwise error is thrown.
+			if(FirebaseApp.getApps().isEmpty()){
+				FirebaseOptions options = new FirebaseOptions.Builder()
+						.setCredentials(GoogleCredentials.fromStream(firebaseCredentials))
+						.build();
+
+				FirebaseApp.initializeApp(options);
+			}
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+        SpringApplication.run(IbiletApplication.class, args);
 	}
 
 }
