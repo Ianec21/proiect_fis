0ec0eb45559c8c01a37356ad85357c43f2a1483f
Ianec21 <jizdan.ianec17@gmail.com>
2024-05-26 23:30:37 +0300

    First upload of the project

---
+++ src/main/java/com/ibilet/IbiletApplication.java
@@ -0,0 +1,13 @@
+package com.ibilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbiletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibilet/IbiletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbiletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
