d36aa5d663fdbb344e5a1345f7d80aeced0b949c
VladDev <vlad.iachim15@gmail.com>
2024-05-29 19:10:30 +0300

    safe save

--- src/main/java/com/ibilet/controllers/FlightFilterController.java
+++ src/main/java/com/ibilet/controllers/FlightFilterController.java
@@ -1,10 +1,16 @@
 package com.ibilet.controllers;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FlightFilterController {
+    @PostMapping("/search")
+    public String searchFlights(Model model,String destination,String arrival,String adults,String children,String start,String flightType){
+        return "/search";
+    }
     @GetMapping("/booking")
     public String openBooking(){
         return "booking";
